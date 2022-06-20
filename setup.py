a0e780aa8f103155caa94caaebd5550401111016
lopez <patrice.lopez@science-miner.com>
2022-06-20 20:58:54 +0200

    adapt setup

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 with open('requirements.txt', 'r') as f:
     reqs = f.readlines()
 
-setup(name='software_mentions_client',
+setup(name='softdata_mentions_client',
       version='0.0.1',
-      description='software_mentions_client',
+      description='Python client for using the Softcite software mention recognition service and the DataStet dataset mention recognition service',
       author='kermitt2',
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       install_requires=reqs,
